118f082f4e198364e2472b25f0e35ef24a30366e
WNjihia <dorisn172@gmail.com>
2020-02-03 19:36:14 +0300

    modify step definitions to accept multiple keywords

---
+++ features/home_screen.feature
@@ -0,0 +1,11 @@
+Feature: Tests for Home screen functionality
+  Scenario: Default values on Home screen is Foot and Centimeter
+    Given I land on home screen
+    Then Left Unit picker value should be "Foot"
+    And Right Unit picker value should be "Centimeter"
+
+  Scenario: Show All button should be enabled at launch
+    Given I land on home screen
+    Then Show All button should be enabled
+    When I press on Clear button
+    Then Show All button should be disabled
---
+++ features/step_definitions/home_screen_steps.rb
@@ -0,0 +1,19 @@
+Then(/^Left Unit picker value should be "([^"]*)"$/) do |string|
+  puts('Left unit picker value is ' + string)
+end
+
+Then(/^Right Unit picker value should be "([^"]*)"$/) do |value|
+  puts('Right unit picker value is ' + value)
+end
+
+Then(/^Show All button should be (enabled|disabled)$/) do |state|
+  if state == 'enabled'
+    puts('Button is enabled')
+  elsif state == 'disabled'
+    puts('Button is disabled')
+  end
+end
+
+When(/^I press on Clear button$/) do
+  puts('Clear button is pressed')
+end
